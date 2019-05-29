2034b57454e420fa3f15b3898248db9fbdb9d015
KarinAharon <44925574+KarinAharon@users.noreply.github.com>
2019-05-29 18:06:08 +0300

    Update signals.c

--- signals.c
+++ signals.c
@@ -1,9 +1,12 @@
-#include<stdio.h> 
-#include<stdlib.h> 
-#include<unistd.h> 
-#include<sys/types.h> 
-#include<string.h> 
-#include<sys/wait.h> 
+#include<stdio.h>
+#include<stdlib.h>
+#include<stdio.h>
+#include<unistd.h>
+#include<sys/types.h>
+#include<string.h>
+#include<wait.h>
+#include<signal.h>
+#include<sys/wait.h>
 
 int main() 
 { 
