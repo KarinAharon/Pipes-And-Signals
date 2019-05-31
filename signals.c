cefefe3d5f18b271d5a775076611014a49426b2a
Oranit Cohen <44542433+Oranit95@users.noreply.github.com>
2019-05-31 13:49:32 +0300

    Update signals.c

--- signals.c
+++ signals.c
@@ -2,53 +2,51 @@
 #include<stdlib.h> 
 #include<unistd.h> 
 #include<sys/types.h> 
+#include<string> 
 #include<sys/wait.h> 
 #include<fcntl.h>
 #include<sys/uio.h>
 #include<sys/types.h>
 #include<signal.h>
 #include<sys/fcntl.h>
+
 int arr[5];
 int pidIndex;
 
-void sigCatcher(int sign){
-      printf("PID %d caught one\n", arr[pidIndex]);
-       if(pidIndex>=0) kill(arr[pidIndex],SIGINT);
-      exit(0);
-}
+
+void sigCatcher(int sign);
 
 int main(){
 
 int p;
 int i;
-signal(SIGINT, sigCatcher);	
+
 
 for (i= 0; i < 5; i++){
   p = fork();
   if(p < 0){  
-    printf("Fork not successfull\n");  
+    printf("Fork was not successfull\n");  
     exit(0);
   }  
   if (p==0){
+    signal(SIGINT, sigCatcher);	
     printf("PID %d is ready\n", getpid());
-
-    //arr[i]=0;
     pidIndex=i-1;
     pause();
     exit(0);
   }
-  else {
-    arr[i]=p;
-  }	
+  else arr[i]=p;
+  	
 } 	
-  sleep(3);
+ sleep(2);
  kill(arr[4],SIGINT);
+ int state;
+ int Zchild;
+ sleep(2);
 
-int state;
-  sleep(3);
 for (int i = 0; i<5 ; i++){
-	int Zchild = wait(&state);
-	printf("process %d is dead\n", Zchild);
+	Zchild = wait(&state);
+	printf("Process %d is dead\n", Zchild);
 	kill(Zchild,SIGTERM);
 }
 
@@ -59,4 +57,9 @@ for (int i = 0; i<5 ; i++){
 
 exit(0);
 
+}
+void sigCatcher(int sign){
+      printf("PID %d caught one\n", getpid());
+      if(pidIndex>=0) kill(arr[pidIndex],SIGINT);
+      exit(0);
 }
